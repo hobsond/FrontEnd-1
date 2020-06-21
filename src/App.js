cbd0f2148713d33885fc5b5c50e94f5aa9c7bc5f
MaryamMosstoufi <maryam@zooxoos.com>
2020-06-21 17:43:40 -0400

    Switch + Routes  + AppMenu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
 
 import AppNav from './components/AppNav.js';
@@ -8,6 +9,23 @@ import PlayQueue from './components/PlayQueue.js';
 function App() {
   return (
     <div className='App'>
+      <Switch>
+        <Route path='/albums'>
+          this is albums page
+        </Route>
+        <Route path='/playlists'>
+          this is playlists page
+        </Route>
+        <Route path='/playing'>
+          this is playing page
+        </Route>
+        <Route path='/favorites'>
+          this is favorites page
+        </Route>
+        <Route path='/'>
+          this is home page
+        </Route>
+      </Switch>
 
       <PlayQueue />
       <footer className="app-footer">
